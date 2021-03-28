24f91002e704d8b7a7afae5c39ae931f64572652
Toxic254 <cyrus_g@outlook.com>
2021-03-28 16:30:46 +0300

    Created form handling partial, worked with scheduling tweets, working on diplaying scheduled tweets and performing CRUD ops.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,27 @@
 class TweetsController < ApplicationController
     before_action :require_user_logged_in!
 
+    def create
+        @tweet = Current.user.tweets.new(tweet_params)
+
+        if @tweet.save
+            redirect_to tweets_path, notice: "Tweet Scheduled Successfully!"
+        else
+            render :new
+        end
+    end
+
     def index
         @tweets = Current.user.tweets
     end
 
     def new 
         @tweet = Tweet.new
+    end
+
+    private
+
+    def tweet_params
+        params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
     end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,11 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
+
+  validates :body, length: { minimum: 5, maximum: 280 }
+  validates :publish_at, presence: true
+
+  after_initialize do
+    self.publish_at ||= 1.hour.from_now
+  end
 end
